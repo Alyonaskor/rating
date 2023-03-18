3cf4697f196252bcec51a7da1f0d0cce5c3ccafc
Alyonaskor <alenaolegovna13@gmail.com>
2023-03-18 20:48:01 +0200

    remove focuse from button buttons of rating

--- src/components/ListCard.js
+++ src/components/ListCard.js
@@ -12,10 +12,13 @@ function ListCard({ heroes }) {
         ))
         setHeroeslist(newList)
       }
-console.log(heroesList);
+
     return (
         <div className="sm:mx-24 mx-10 text-center">
             <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Top 10</h2>
+
+
+
             <div className="mx-auto grid max-w-2xl auto-rows-fr grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-3">
                 {heroesList.sort((a, b) => b.rating - a.rating).map((hero, index) => (
                     <Card key={index} hero={hero} getRating={updateRating}/>
--- src/components/SelectRating.js
+++ src/components/SelectRating.js
@@ -6,13 +6,12 @@ function SelectRaiting({handleClick}) {
       <p className="text-indigo-400">How do you rait this single?</p>
         <div className="flex justify-around ">
           {Array.from({ length: 10 }, (_, i) => (
-            <div key={`${i + 1}`}>
+            <div key={`${i + 1}`} className="">
               <button
-                type="radio"
                 value={`${i + 1}`}
                 onClick={handleClick}
-                className="text-center ml-1 h-6 w-6 border-gray-300 text-gray-600 focus:bg-indigo-600 focus:text-white" >
-                <div className="text-sm font-medium" >
+                className="text-center ml-1 h-6 w-6 border-gray-300 text-gray-600 " >
+                <div className="text-sm font-medium border rounded-full w-6 h-6 bg-white" >
                   {i + 1}
                 </div>
               </button>
