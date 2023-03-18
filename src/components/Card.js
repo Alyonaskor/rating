633652b2f24db8d249e465532c3735e1296cc06d
Alyonaskor <alenaolegovna13@gmail.com>
2023-03-18 17:04:35 +0200

    change rating onClick

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,13 +1,18 @@
 import SelectRating from "./SelectRating";
-// import { useState } from "react";
+import { useState } from "react";
 
 export default function Card({ hero }) {
+    const [selected, setSelected] = useState(hero.rating)
   
+    const handleClick = (e) => {
+      setSelected(e.currentTarget.value)
+    }
+
     return (
         <div className="mt-12">
-            <span className="-my-6 -mx-4 z-30 relative flex h-14 w-14 items-center justify-center rounded-full bg-indigo-600 text-white">
-                {hero.rating}
-            </span> 
+            <span className="-my-6 -mx-4 z-30 relative flex h-14 w-14 items-center justify-center rounded-full bg-indigo-600 text-white text-2xl">
+                {selected}
+            </span>
             <div className="relative isolate flex flex-col justify-end overflow-hidden rounded-2xl bg-gray-900 px-8 pb-8 pt-80 sm:pt-48 lg:pt-80 leading-10">
                 <img src={hero.coverUrl} alt="" className="absolute inset-0 -z-10 h-full w-full object-cover" />
                 <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40" />
@@ -21,7 +26,7 @@ export default function Card({ hero }) {
                 <div className="text-white mb-4">
                     <span className="absolute  " />Single: {hero.single}
                 </div>
-                <SelectRating />
+                <SelectRating rating={hero.rating} handleClick={handleClick}/>
             </div>
         </div>
     )
--- src/components/SelectRating.js
+++ src/components/SelectRating.js
@@ -1,18 +1,28 @@
+import { useState } from "react"
 
-function SelectRaiting() {
-  
+
+
+function SelectRaiting({rating,  handleClick}) {
+  // const [selected, setSelected] = useState(rating)
+
+
+  // const handleChange = (e) => {
+  //   setSelected(e.currentTarget.value);
+  // };
+
+
   return (
     <div>
 
       <p className="text-gray-400">How do you rait this single?</p>
-      <div className="mt-4">
-        <div className="flex  justify-around ">
+        <div className="flex justify-around ">
           {Array.from({ length: 10 }, (_, i) => (
             <div key={`${i + 1}`}>
               <button
                 type="radio"
                 value={`${i + 1}`}
-                className="text-center h-6 w-6 border-gray-300 text-gray-600 focus:bg-indigo-600 focus:text-white" >
+                onClick={handleClick}
+                className="text-center ml-1 h-6 w-6 border-gray-300 text-gray-600 focus:bg-indigo-600 focus:text-white" >
                 <div className="text-sm font-medium" >
                   {i + 1}
                 </div>
@@ -20,7 +30,6 @@ function SelectRaiting() {
             </div>
           ))}
         </div>
-      </div>
     </div>
   )
 }
