40233172454aaee1fbf7f86392726b8f797e5209
Alyonaskor <alenaolegovna13@gmail.com>
2023-03-27 23:49:30 +0300

    img  size for mobile

--- src/components/Card.js
+++ src/components/Card.js
@@ -6,15 +6,15 @@ export default function Card({index, hero, updateRating }) {
     return (
         <div className=" flex justify-between bg-[#FFFFFF] md:h-[150px]  mx-2 md:mx-0 mb-3 md:mb-[18px]">
             <div className="flex">
-                <div className="mr-2 md:mr-[52px] md:w-[266px]">
+                <div className="mr-2 md:mr-[52px] md:w-[266px]  ">
                     <img 
-                    className="h-20 md:h-[150px]"
+                    className=" md:h-[150px] "
                     src={hero.coverUrl} 
                     alt={hero.name}></img>
                 </div>
-                <div className="h-10 md:h-[116px] mt-2 md:mt-[19px] md:mb-[15px] ">
-                    <p style={{ color: hero.color }} className="text-start font-raleway text-base md:font-raleway-xl md:text-xl"> {hero.name}</p>
-                    <p className="text-start font-raleway text-xs md:text-base my-1 md:my-3"> {hero.single}</p>
+                <div className="h-10 md:h-[116px] md:mt-[19px] md:mb-[15px] ">
+                    <p style={{ color: hero.color }} className="text-start font-raleway text-sm md:font-raleway-xl md:text-xl"> {hero.name}</p>
+                    <p className="text-start font-raleway text-xs md:text-base md:my-3"> {hero.single}</p>
                     <RatingStars   index={index} updateRating={updateRating} hero={hero} />
                 </div> 
             </div>
